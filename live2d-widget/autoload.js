7880fb1839aa70a4bffe26bc52aa70defad189d4
zhangxiaotongbu <478361776@qq.com>
2021-12-23 16:11:46 +0800

    Site updated: 2021-12-23 16:11:46

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -35,8 +35,7 @@ if (screen.width >= 768) {
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-			//cdnPath: "https://unpkg.zhimg.com/akilar-live2dapi@latest/"
-			apiPath: "/live2d-widget/api/"
+			cdnPath: "https://unpkg.zhimg.com/akilar-live2dapi@latest/"
 		});
 	});
 }
